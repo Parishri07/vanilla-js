b75f0a6ffda4f611a3c53cdb845a18c6da0ae4da
Parishri Shah <136893851+Parishri07@users.noreply.github.com>
2023-09-17 22:30:32 +0530

    Update DB.js

--- services/DB.js
+++ services/DB.js
@@ -1,49 +1,61 @@
 const DB = {
-    init(){
-        const defaultValues = {
-            default: ['default todo'],
-        }
+  init() {
+    // Initialize the database with default values if it's empty.
+    const defaultValues = {
+      default: ['default todo'],
+    };
 
-        const todos = JSON.parse(localStorage.getItem('todos'))
+    const todos = JSON.parse(localStorage.getItem('todos'));
 
-        if(!todos){
-         this.saveTodos(defaultValues)
-        }
-
-        window.todos = todos
-    },
-
-    getTodos(){
-        return JSON.parse(localStorage.getItem('todos'))
-    },
-
-    saveTodos(todos){
-        localStorage.setItem('todos', JSON.stringify(todos))
-    },
-
-    addTodo(todo, category){
-        const todos = this.getTodos()
-        if(!todos[category]){
-            todos[category] = []
-        }
-
-        todos[category].push(todo)
-
-        this.saveTodos(todos)
-    },
-
-    deleteTodo(todo, category){
-        const todos = this.getTodos()
-        if(!todos[category]){
-            return false
-        }
-        todos[category] = todos[category].filter((td)=>{
-            if(todo == td) return false
-            else return true
-        })
-
-        this.saveTodos(todos)
+    if (!todos) {
+      // If there are no todos in localStorage, save the default ones.
+      this.saveTodos(defaultValues);
     }
-}
 
-export default DB
+    // Make the todos data accessible via the `window` object.
+    window.todos = todos;
+  },
+
+  getTodos() {
+    // Retrieve todos from localStorage and parse it into an object.
+    return JSON.parse(localStorage.getItem('todos'));
+  },
+
+  saveTodos(todos) {
+    // Save the todos object to localStorage after converting it to a JSON string.
+    localStorage.setItem('todos', JSON.stringify(todos));
+  },
+
+  addTodo(todo, category) {
+    const todos = this.getTodos();
+    if (!todos[category]) {
+      // If the category doesn't exist, create an empty array for it.
+      todos[category] = [];
+    }
+
+    // Add the new todo to the specified category.
+    todos[category].push(todo);
+
+    // Save the updated todos object to localStorage.
+    this.saveTodos(todos);
+  },
+
+  deleteTodo(todo, category) {
+    const todos = this.getTodos();
+    if (!todos[category]) {
+      // If the category doesn't exist, return false (no deletion).
+      return false;
+    }
+
+    // Use the filter method to remove the specified todo from the category.
+    todos[category] = todos[category].filter((td) => {
+      if (todo == td) return false;
+      else return true;
+    });
+
+    // Save the updated todos object to localStorage.
+    this.saveTodos(todos);
+  },
+};
+
+export default DB;
